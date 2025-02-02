01e6d3a88ccd979197fa39bb6e305a0e13c7bcd4
Giann Pabon <giannalberto2@outlook.com>
2025-02-02 15:54:20 -0400

    Fix comments for task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+""""
+1. Square with size
+A class Sqaure that defines a sqaure
+"""
+
+
 class Square:
-    """Class that defines a square."""
+    """Class Square that defines a square"""
+
     def __init__(self, size):
+        """Define Private instance attribute: size"""
+
         self.__size = size
