5055b8ce43cebe82acaa1a7d7b77d05dc3e63b5e
Giann Pabon <giannalberto2@outlook.com>
2025-02-09 17:25:38 -0400

    Add task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Defines an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Returns the area of the geometry."""
+        raise Exception("area() is not implemented")
