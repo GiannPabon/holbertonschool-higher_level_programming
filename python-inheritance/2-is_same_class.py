1985132393ffd285e1e778d084d4a7a5ae1ad458
Giann Pabon <giannalberto2@outlook.com>
2025-02-09 16:23:53 -0400

    Fix pycodestyle for task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 2. Same class
-A function that returns True if the object is exactly an instance of the 
+A function that returns True if the object is exactly an instance of the
 specified class otherwise False
-""" 
+"""
+
 
 def is_same_class(obj, a_class):
     """
