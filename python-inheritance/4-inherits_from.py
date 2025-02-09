9da8ad7e440a2bbaefffe548fe544fb5410fb5d1
Giann Pabon <giannalberto2@outlook.com>
2025-02-09 17:05:26 -0400

    Documentation error fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """
-4. Only sub class of
-A function that returns True if the object is an instance of a
-class that inherited (directly or indirectly) from the specified class;
-otherwise False.
+Defines a function.
 """
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """
     Checks if an object is an instance of a class and
     not exactly an instance of the class.
@@ -20,4 +17,4 @@ def is_kind_of_class(obj, a_class):
         True: if obj is an instance of a_class.
         False: if obj is not an instance of a_class.
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_classi
