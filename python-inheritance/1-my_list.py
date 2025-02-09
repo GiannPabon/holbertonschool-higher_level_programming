8acb8329fbea2055224f4a35f783e2e3e6068b1e
Giann Pabon <giannalberto2@outlook.com>
2025-02-09 16:14:15 -0400

    Update documentation in 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,9 +6,13 @@ A class MyList that inherits from list
 
 
 class MyList(list):
-    """Class MyList that inherits from list"""
+    """Class MyList that inherits from list
+    Args:
+        list
+    """
 
     def print_sorted(self):
-        """Prints the list sorted in ascending order"""
-
+        """
+        Prints the list sorted in ascending order
+        """
         print(sorted(self))
